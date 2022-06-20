7bf70ce2f2ac2e26750b425ce015176ad88215fb
ekertree <ekertree@qq.com>
2022-06-20 15:40:56 +0800

    feat(公共工具模块): 统一返回结果

---
+++ common/common-utils/src/main/java/com/ekertree/commonutils/Result.java
@@ -0,0 +1,76 @@
+package com.ekertree.commonutils;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ClassName: Result
+ * Description:
+ * date: 2022/6/20 10:26
+ *
+ * @author Ekertree
+ * @since JDK 1.8
+ */
+@Data
+public class Result {
+
+    @ApiModelProperty(value = "是否成功")
+    private boolean success;
+
+    @ApiModelProperty(value = "状态码")
+    private Integer code;
+
+    @ApiModelProperty(value = "返回消息")
+    private String message;
+
+    @ApiModelProperty("返回的数据")
+    private Map<String,Object> data = new HashMap<>();
+
+    private Result(){}
+
+    public static Result ok(){
+        Result result = new Result();
+        result.setSuccess(true);
+        result.setCode(ResultCode.SUCCESS);
+        result.setMessage("成功");
+        return result;
+    }
+
+    public static Result error(){
+        Result result = new Result();
+        result.setSuccess(false);
+        result.setCode(ResultCode.ERROR);
+        result.setMessage("失败");
+        return result;
+    }
+
+    public Result data(String key,Object value) {
+        this.data.put(key, value);
+        return this;
+    }
+
+    public Result data(Map<String,Object> map) {
+        this.setData(map);
+        return this;
+    }
+
+    public Result setSuccess(boolean success) {
+        this.success = success;
+        return this;
+    }
+
+    public Result setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+
+    public Result setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+}
---
+++ common/common-utils/src/main/java/com/ekertree/commonutils/ResultCode.java
@@ -0,0 +1,14 @@
+package com.ekertree.commonutils;
+
+/**
+ * ClassName: ResultCode
+ * Description:
+ * date: 2022/6/20 10:23
+ *
+ * @author Ekertree
+ * @since JDK 1.8
+ */
+public interface ResultCode {
+    public static final Integer SUCCESS = 200;
+    public static final Integer ERROR = 400;
+}
