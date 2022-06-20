37e1c8bc7a31e3f688bda364e4e8fddc84118032
ekertree <ekertree@qq.com>
2022-06-20 15:48:05 +0800

    feat(公共基础模块): mybatis自动填充功能handler

---
+++ common/service-base/src/main/java/com/ekertree/servicebase/handler/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.ekertree.servicebase.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * ClassName: MyMetaObjectHandler
+ * Description:
+ * date: 2022/6/20 14:31
+ *
+ * @author Ekertree
+ * @since JDK 1.8
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
